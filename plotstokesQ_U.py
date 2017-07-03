13122305f7260215efa03f5238925e2a0bea3e97
Kiana Henny <hennykf@whitman.edu>
2017-07-03 15:27:43 -0700

    is generalized to use files other than fits

--- plotstokesQ_U.py
+++ plotstokesQ_U.py
@@ -11,46 +11,13 @@ from matplotlib.colors import LogNorm
 import os
 import plothealpix_map
 import healpix_to_RA_dec
+import fits_data_extraction
 
 
 def plotstokesQ_U(filename_Q, filename_U, plotfile_base=None, plotdirectory=None,
                   save_show='show', projection='ortho'):
-    if not filename_U.endswith('.fits'):
-        raise ValueError('Stokes U file is not a fits file. Files must be .fits files')
-    if not filename_Q.endswith('.fits'):
-        raise ValueError('Stokes Q file is not a fits file. Files must be .fits files')
-    # get header and data info from the fits file
-    contents_Q = fits.open(filename_Q)
-    pixelnum_Q = contents_Q[1].header['naxis1']
-    data_Q = contents_Q[1].data
-    nside_Q = contents_Q[1].header['nside']
-    ordering_Q = contents_Q[1].header['ordering']
-
-    contents_U = fits.open(filename_U)
-    pixelnum_U = contents_U[1].header['naxis1']
-    data_U = contents_U[1].data
-    nside_U = contents_U[1].header['nside']
-    ordering_U = contents_U[1].header['ordering']
-
-    if pixelnum_U != pixelnum_Q:
-        raise ValueError('files do not have same number of pixels.')
-
-    if nside_U != nside_Q:
-        raise ValueError('files do not have nside.')
-
-    if ordering_U != ordering_Q:
-        raise ValueError('files do not have nside.')
-
-    # extract data from specified files
-    pixels_Q = data_Q.field('PIXEL')
-    signal_Q = data_Q.field('SIGNAL')
-    pixels_U = data_U.field('PIXEL')
-    signal_U = data_U.field('SIGNAL')
-
-    if np.any(pixels_Q != pixels_U):
-        raise ValueError('files do not have set of pixels.')
-
     # Finding x and y from Stokes parameters U and Q
+    signal_Q, signal_U, nside_Q, pixels_Q, ordering_Q = fits_data_extraction.fits_extractor(filename_Q, filename_U)
     Q = signal_Q
     U = signal_U
     K = np.sqrt(U**2 + Q**2)
@@ -73,11 +40,14 @@ def plotstokesQ_U(filename_Q, filename_U, plotfile_base=None, plotdirectory=None
         os.makedirs(plotdirectory)
     plotfile_base = os.path.join(plotdirectory, plotfile_base)
     plotfile = plotfile_base + '.png'
-    # manipulate histogram for number of bins, color scheme.
+
     if filename_Q.endswith('.fits'):
-        healpix_to_RA_dec.healpix_to_RA_dec(nside_Q, pixels_Q, ordering=ordering_Q)
+
+        ra, dec = healpix_to_RA_dec.healpix_to_RA_dec(nside_Q, pixels_Q, ordering=ordering_Q)
     plothealpix_map.mapping(ra, dec, plotfile, theta, projection=projection,
                             save_show=save_show)
+
+    # manipulate histogram for number of bins, color scheme.
     plt.hist2d(x_stokes, y_stokes, bins=150, norm=LogNorm())
     plt.colorbar()
 
