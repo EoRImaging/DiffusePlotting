1e9783930d376d819ff6efcd5262a4874cb51509
jpcanright <jpcanright@gmail.com>
2017-07-14 15:22:10 -0700

    Updates, documentation, bug fixes, and improvements to Gauss plotter.

--- gtools.py
+++ gtools.py
@@ -1,26 +1,30 @@
 from gaussfuncs import makeGauss
 import healpy as hp
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 from derp import *
 import os, errno, re
 import cartopy.crs as ccrs
 
-# Hard-coded quantities; to be fixed
-#fname = './catalogs/1130788624_source_array.sav'
-#variances = [10000.0, 30000.0]
-#nside = 1024
-#obsID = fname[11:21] # TODO: replace with smarter regexp
-
 def crdir(directory):
+    """
+crdir - Create directory. Creates a directory if it doesn't already exist.
+If the directory already exists, it handles the error intelligently.
+If the directory cannot be created for any other reason, an error is raised.
+Input: string containing path of directory to be created. 
+Returns nothing.   
+    """
     try: 
         os.makedirs(directory)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+### END def crdir
 
-def numpize(fname):
+
+def numpize(fname,saveData):
     sourceList = buildSources(fname)
     print('Sources loaded')
     flag = 0
@@ -38,22 +42,14 @@ def numpize(fname):
     raout = dataArray[:,0]
     decout = dataArray[:,1]
     fluxout = dataArray[:,2]
-    np.savez_compressed(fname[:-3]+'npz',ra=raout,dec=decout,flux=fluxout)
+    if saveData:
+        np.savez_compressed(fname[:-3]+'npz',ra=raout,dec=decout,flux=fluxout)
     return raout, decout, fluxout
 
-def gaussify(sra, sdec, sflux, var, nside, obsID):
+def gaussify(sra, sdec, sflux, var, nside, obsID, saveData):
     # Gaussed data output file name
     gfname='./gaussdata/'+obsID+'_gauss_nside'+str(nside)+'_var'+str(var)+'.npz'
 
-    #if not(os.path.isfile(gfname)):
-    #if os.path.isfile(fname):
-    #    npzfile = np.load('catrun1.npz')
-    #    sra = npzfile['ra']
-    #    sdec = npzfile['dec']
-    #    sflux = npzfile['flux']
-    #else:
-    #    sra, sdec, sflux = numpize(fname)
-    
     # Normalize to 1.0 to avoid getting negative numbers from log10
     # TODO: Implement +=1, multiplicative normalization switch, compare
     print('Min flux: '+str(min(sflux)))
@@ -61,23 +57,40 @@ def gaussify(sra, sdec, sflux, var, nside, obsID):
     normConst = 1.0/min(sflux)
     print('Normalizing flux to 1.0 by multiplying by '+str(normConst))
     sflux *= normConst
-    bounds = [min(sra)-5.0, max(sra)+5.0, min(sdec)-5.0, max(sdec)+5.0]
-    ra, dec, dist, amp = makeGauss(nside,sra,sdec,np.log10(sflux),var,bounds=bounds)
-    print('Saving ' + gfname)
-    np.savez_compressed(gfname,ra=ra,dec=dec,amp=amp,dist=dist)
-    return ra, dec, amp, dist
-    #    gaussplot(ra,dec,flux,100.0,var,nside,obsID)
+    
+    ra, dec, amp = makeGauss(nside,sra,sdec,np.log10(sflux),var)
+    #print('[gaussify] Values returned from makeGauss: ')
+    #print('RA: ' + str(ra))
+    #print('dec: ' + str(dec))
+    #print('amp: ' + str(amp))
+    if saveData:
+        print('Saving ' + gfname)
+        np.savez_compressed(gfname,ra=ra,dec=dec,amp=amp)
+
+    return ra, dec, amp
     ### END def gaussify
 
-def gaussplot(ra,dec,amp,cap,var,nside,obsID,savePlot=True):
+def gaussplot(ra,dec,amp,cap,var,nside,obsID,savePlot,colorbarLevels):
+    print('[gaussplot] Brightest single point has amplitude ' + str(max(amp)))
+    # Set figure size to be nice and big
+    mpl.rcParams['figure.figsize'] = [24.0, 18.0]
+    
     proj = ccrs.PlateCarree()
     ax = plt.axes(projection=proj)
-    plt.tricontourf(ra,dec,np.clip(amp,None,cap),100,cmap='Greys',transform=proj)
+    #plt.tricontourf(ra,dec,np.clip(amp,None,cap),colorbarLevels,cmap='Greys',transform=proj)
+    #print('Parameters passed into gaussplot:')
+    #print('RA: ' + str(ra))
+    #print('dec: ' + str(dec))
+    #print('amp: ' + str(amp))
+    plt.scatter(ra,dec,c=np.clip(amp,None,cap),s=0.0001,cmap='Greys',alpha=0.2,transform=proj)
     plt.colorbar()
-    
+    plt.gca().invert_xaxis()
     # Label the plot all pretty-like
-    plt.ylabel('Declination ($^\circ$)')
-    plt.xlabel('Right ascension ($^\circ$)')
+    #plt.ylabel('Declination ($^\circ$)')
+    #plt.xlabel('Right ascension ($^\circ$)')
+    # Awful hacky workaround for axis labels because Cartopy sucks
+    ax.set_xlabel('Right ascension ($^\circ$)')
+    ax.set_ylabel('Declination ($^\circ$)')
     plt.title('obsID: '+obsID+'\n var = '+str(var)+'$^\circ$ cap = '+str(cap)+' nside='+str(nside))
     #ax.set_xticks([45,60,75,90,105,120])
     #ax.set_yticks([-20,-30,-40,-50,-60,-70])
@@ -97,36 +110,130 @@ def gaussplot(ra,dec,amp,cap,var,nside,obsID,savePlot=True):
         plotname = './gaussplots/'+obsID+'_nside'+str(nside)+'_var'+str(var)+'_cap' + str(cap) + '_projected.png'
         print('Saving '+plotname)
         plt.savefig(plotname)
+        print('Saved.')
         plt.close()
         return
     else:
         print('ACK NOT IMPLEMENTED DO NOT DO THAT')
-        return plt, ax, gl
+        return
+        #return plt, ax, gl
 #   
 
-def makeGaussPlot(fname, nside, var, cap,saveData=True):
-    # Create output directories if they don't already exist
-    crdir('./gaussdata')
-    crdir('./gaussplots')
+def makeFromScratch(fname, nside, var, cap, obsID, saveData, savePlot, 
+                    colorbarLevels):
+    print('Extracting ra, dec, flux (I) from IDL .sav file')
+    sra, sdec, sflux = numpize(fname, saveData)
+    print('Computing Gaussians')
+    ra, dec, amp = gaussify(sra, sdec, sflux, var, nside, obsID, saveData)
+    print('Plotting Gaussians')
+    gaussplot(ra, dec, amp, cap, var, nside, obsID, savePlot, colorbarLevels)
+    print('makeGaussPlot done')
+    return
     
+def handleFuckery(fname, nside, var, cap, obsID, saveData, loadData, savePlot, 
+                  colorbarLevels):
+    print('')
+    print('makeGaussPlot started using following parameters:\n')
+    print('obsID (GPS seconds): ' + obsID)
+    print('nside - HealPIX grid resolution: ' + str(nside))
+    print('var (degrees) - Variance for each Gaussian: ' + str(var))
+    print('cap (arb. units) - Upper limit on plotted values: ' + str(cap))
+    if colorbarLevels > 998:
+        raise ValueError('Cannot have more than 998 color levels in output contour plot.')
+        #colorbarLevels = 998
+    elif colorbarLevels < 0:
+        raise ValueError('Cannot have negative number of colorbar levels.')
+        #colorbarLevels = 10
+    else:
+        print('Output colorbar levels: ' + str(colorbarLevels))
+    if saveData:
+        print('saveData is True; numpy ra/dec/flux arrays for sources and their Gaussian convolution will be saved under ./gaussdata')
+    else:
+        print('saveData is False; no intermediate data will be saved to disk.')
+    if loadData:
+        print('loadData is True; existing ra/dec/flux numpy arrays of Gaussian-convolved or catalog data will be loaded if available.')
+    else:
+        print('loadData is False; the plot will be created from scratch starting with the .sav file.')
+    if savePlot:
+        print('savePlot is True; the plot will be saved under ./gaussplots')
+    else:
+        raise NotImplementedError('savePlot is False; this option is not implemented.')
+        #savePlot = True
+    print('')
+
+
+def makeGaussPlot(fname, nside, var, cap, saveData=True, loadData=True, 
+                  savePlot=True, colorbarLevels=50):
+    """Make truncated-Gaussian plot of EoR catalog .sav data.
+    
+    Arguments:
+    fname -- Path to IDL .sav file of source catalog data to plot
+    nside -- Resolution of HealPIX grid to convolve & plot upon
+    var -- Variance of Gaussian in decimal degrees
+    cap -- Upper limit on plotted values, arbitrary units
+    
+    Keyword arguments: TODO
+    """
     # Extract obsID from fname using newbie regex
     # DEBUGGING NOTE: Any instance of '113' elsewhere in the file name followed
     # by 7 digits will cause an error!
     searchObj = re.search( r'113\d{7}', fname)
     obsID = searchObj.group()
-    print('obsID: ' + obsID)
+    handleFuckery(fname, nside, var, cap, obsID, saveData, loadData, savePlot, colorbarLevels)
+    #print('')
+    #print('makeGaussPlot started using following parameters:\n')
+    #print('obsID (GPS seconds): ' + obsID)
+    #print('nside - HealPIX grid resolution: ' + str(nside))
+    #print('var (degrees) - Variance for each Gaussian: ' + str(var))
+    #print('cap (arb. units) - Upper limit on plotted values: ' + str(cap))
+    #if colorbarLevels > 998:
+    #    print('Cannot have more than 998 levels in output contour plot. Reducing from ' + str(colorbarLevels) + ' to 998.')
+    #    colorbarLevels = 998
+    #elif colorbarLevels < 0:
+    #    print('Cannot have negative number of colorbar levels. Defaulting to 10.')
+    #    colorbarLevels = 10
+    #else:
+    #    print('Output colorbar levels: ' + str(colorbarLevels))
+    #if saveData:
+    #    print('saveData is True; numpy ra/dec/flux arrays for sources and their Gaussian convolution will be saved under ./gaussdata')
+    #else:
+    #    print('saveData is False; no intermediate data will be saved to disk.')
+    #if loadData:
+    #    print('loadData is True; existing ra/dec/flux numpy arrays of Gaussian-convolved or catalog data will be loaded if available.')
+    #else:
+    #    print('loadData is False; the plot will be created from scratch starting with the .sav file.')
+    #if savePlot:
+    #    print('savePlot is True; the plot will be saved under ./gaussplots')
+    #else:
+    #    print('savePlot is False; this option is not implemented, so savePlot is being set to True.')
+    #    savePlot = True
+    #print('')
+    
+    # Create output directories if they don't already exist
+    if saveData:
+        crdir('./gaussdata')
+    if savePlot:
+        crdir('./gaussplots')
+    
+    # Make plots from scratch if loadData is False
+    if not(loadData):
+        makeFromScratch(fname, nside, var, cap, obsID, saveData, savePlot, colorbarLevels)
+        return
+    
+    
     # Gauss'd file name
     gfname='./gaussdata/'+obsID+'_gauss_nside'+str(nside)+'_var'+str(var)+'.npz'
     
     # If Gauss'd output file already exists, just plot it
     if os.path.isfile(gfname):
-        print('Loading existing Gaussed healpix data')
+        print('Loading existing Gaussian-convolved healpix data')
         npdict = np.load(gfname)
         ra = npdict['ra']
         dec = npdict['dec']
         amp = npdict['amp']
         print('Plotting Gaussians')
-        gaussplot(ra, dec, amp, cap, var, nside, obsID)
+        print('Plot data length: ' + str(len(amp)))
+        gaussplot(ra, dec, amp, cap, var, nside, obsID, savePlot, colorbarLevels)
         print('makeGaussPlot done')
         return
     else: # If Gauss'd data doesn't exist:
@@ -138,44 +245,14 @@ def makeGaussPlot(fname, nside, var, cap,saveData=True):
             sdec = npdict['dec']
             sflux = npdict['flux']
             print('Computing Gaussians')
-            ra, dec, amp, dist = gaussify(sra, sdec, sflux, var, nside, obsID)
+            ra, dec, amp = gaussify(sra, sdec, sflux, var, nside, obsID, saveData)
             print('Plotting Gaussians')
-            gaussplot(ra,dec,amp,cap,var,nside,obsID)
+            gaussplot(ra,dec,amp,cap,var,nside,obsID,savePlot,colorbarLevels)
             print('makeGaussPlot done')
             return
-        else:
-            print('Extracting ra, dec, flux (I) from IDL .sav file')
-            sra, sdec, sflux = numpize(fname)
-            print('Computing Gaussians')
-            ra, dec, amp, dist = gaussify(sra, sdec, sflux, var, nside, obsID)
-            print('Plotting Gaussians')
-            gaussplot(ra, dec, amp, cap, var, nside, obsID)
-            print('makeGaussPlot done')
+        else: # If np format ra, dec, flux don't exist, extract from sav file
+            makeFromScratch(fname, nside, var, cap, obsID, saveData, savePlot, colorbarLevels)
             return
-        
 
-#gfname='./catalogs/'+obsID+'_gauss_nside'+str(nside)+'_var'+str(var)+'.npz'
-
-#if not(os.path.isfile(gfname)):
-#    if os.path.isfile(fname):
-#        npzfile = np.load('catrun1.npz')
-#        sra = npzfile['ra']
-#        sdec = npzfile['dec']
-#        sflux = npzfile['flux']
-#    else:
-#        sra, sdec, sflux = numpize(fname)
-    
-
-    # TODO: Implement +=1, multiplicative normalization switch, evaluatei
-#    print('Min flux: '+str(min(sflux)))
-#    print('Max flux: '+str(max(sflux)))
-#    normConst = 1.0/min(sflux)
-#    print('Normalizing to 1.0 by multiplying by '+str(normConst))
-#    sflux *= normConst
-#    for var in variances:#, 3000.0, 10000.0, 300.0, 100000.0, 300000.0]:
-#        ra, dec, dist, amp = makeGauss(nside,sra,sdec,np.log10(sflux),var,bounds=[15,80,-70,-20])
-#        print('Pre-logged Gauss grid generated for variance '+str(var))
-#        np.savez_compressed(gfname,ra=ra,dec=dec,dist=dist,amp=amp)
-#        gaussplot(ra,dec,flux,100.0,var,nside,obsID)
-#
+# A cool thing Bryna did that I may need to reference sometime
 #ra[np.where(ra > 180)] -= 360
