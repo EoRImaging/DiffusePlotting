13206cd9984903d70b9fa92366339a857ab4b018
jpcanright <jpcanright@gmail.com>
2017-07-25 09:04:16 -0700

    Multiple-color plotting functionality, debugging tools

--- gaussfuncs.py
+++ gaussfuncs.py
@@ -1,11 +1,12 @@
 # gaussfuncs.py - contains functions for plotting Gaussian functions on
 # HealPIX pixel maps.
-from math import exp
+#from math import exp
 import healpy as hp
 import numpy as np
 from havnp import haversine
 import itertools
 from numba import autojit
+from jdb import *
 
 @autojit
 def computeGauss(amp, dist, var):
@@ -18,40 +19,39 @@ def computeGauss(amp, dist, var):
     return prod
 
 @autojit    
-def makeGauss(nside, ra, dec, amp, varparam=np.sqrt(2*np.pi)): #bounds=None):#radius=6447797.0):
-    print('RA length:' + str(len(ra)))
-    print('dec length:' + str(len(dec)))
-    print('amp length:' + str(len(amp)))
+def makeGauss(nside, ra, dec, amp, varparam=np.sqrt(2*np.pi),radius=5.0): #bounds=None):#radius=6447797.0):
+    #print('RA length:' + str(len(ra)))
+    #print('dec length:' + str(len(dec)))
+    #print('amp length:' + str(len(amp)))
+    radius = radius/60.0
     var = varparam*1.0
-    #raMin = min(ra)-2.0
-    #raMax = max(ra)+2.0
-    #decMin = min(dec)-2.0
-    #decMax = max(dec)+2.0
-    #m = np.arange(hp.nside2npix(nside))
-    #vertices = hp.ang2vec([raMin,raMin,raMax,raMax],[decMin,decMax,decMax,decMin],lonlat=True)
-    #m = hp.query_polygon(nside,vertices)
-    #m_ra, m_dec = hp.pix2ang(nside, m, lonlat=True)
-    #m_amp = np.zeros_like(m,dtype='float64')
-    #ra = ra*1.0
-    #dec = dec*1.0
-    #numpix = m.size
     out_ra = np.array([])
     out_dec = np.array([])
     out_amp = np.array([])
-    ### TO IMPLEMENT: Check ra, dec, amp are of same size
+    ### TODO: Check ra, dec, amp are of same size
     #if isinstance(ra, np.ndarray):
+    numSources = len(ra)
+    index = 0
+    jndex = 0
+    interval = numSources/10
     for rai, deci, ampi in itertools.izip(ra, dec, amp):
+        vdiag('makeGauss',rai=rai,deci=deci,ampi=ampi)
+        if index == jndex:
+            print(str(jndex) + ' convolutions of ' + str(numSources) + ' complete.')
+            jndex += interval
+        index += 1
         # Used to convert from ra/dec to pixel to vec; should not be necessary
         #nearpix = hp.pix2vec(nside,hp.ang2pix(nside,rai,deci,lonlat=True))
         nearpix = hp.ang2vec(rai,deci,lonlat=True)
         # Calculate Gaussian on pixels within following radius (in degrees)
-        radius = 3.0/60.0 # Hard-coded; remove eventually
+        #radius = 15.0/60.0 # TODO Hard-coded; remove eventually
         pxls = hp.query_disc(nside,nearpix,radius*np.pi/180.0)
         near_ra, near_dec = hp.pix2ang(nside, pxls, lonlat=True)
         dist = haversine(np.full_like(near_ra,rai), np.full_like(near_dec,deci), near_ra, near_dec)
         out_ra = np.append(out_ra,  near_ra)
         out_dec = np.append(out_dec, near_dec)
         out_amp = np.append(out_amp, computeGauss(ampi, dist, var))
+        vdiag('makeGauss',out_ra=out_ra,out_dec=out_dec,out_amp=out_amp)
         #print('Using nearest '+str(len(near_ra))+' pixels to source.')
     print('makeGauss output length: '+str(out_amp.size))
     return out_ra, out_dec, out_amp
--- tooltest.py
+++ tooltest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from gtools import *
 import resource
 from datetime import datetime
@@ -9,10 +10,14 @@ startTime = datetime.now()
 
 names = ['./catalogs/1130788624_source_array.sav']#, './catalogs/1130784064_source_array.sav', './catalogs/1130781304_source_array.sav']
 nside = 4096
-variances = [0.1]#, 0.1, 0.03, 0.01]
-caps = [5.0, 4.0, 3.0, 2.0, 1.0]
+variances = [0.05, 0.1, 0.15]#, 0.1, 0.03, 0.01]
+caps = [1.0, 1.5, 2.0] #[2.0, 1.5, 1.0]
+radii = [5.0, 10.0, 15.0, 20.0]
+sources = [[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1]]
 for fname in names:
     for var in variances:
         for cap in caps:
-            makeGaussPlot(fname, nside, var, cap, loadData = True, colorbarLevels=10)
+            for radius in radii:
+                for src in sources:
+                    makeGaussPlot(fname, nside, var, cap, saveData=True, loadData = False, points=src[0], centroids=src[1], components=src[2], cmaps=['Reds','Blues','Greens'], alpha=[1.0,1.0,1.0],radius=radius)
 print('tooltest.py executed in '+str(datetime.now()-startTime)+' seconds.')
